b37d1233b2b631bc9930ff6c7832ebbba3adac28
Tun Cham Roeun <chamroeuntun25@outlook.com>
2023-09-13 09:50:12 +0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# mix-format README
+
+run `mix format` for elixir
+
+## Features
+
+Help running `mix format` to format file
+
+## Requirements
+
+1 - Installed `elixir`
---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+import { languages, ExtensionContext } from "vscode";
+import {
+  workspace,
+  commands,
+  Range,
+  TextDocument,
+  TextEdit,
+  window,
+} from "vscode";
+import cp = require("child_process");
+
+function fullDocumentRange(document: TextDocument): Range {
+  const lastLineId = document.lineCount - 1;
+  return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+}
+
+function format(document: TextDocument): Promise<TextEdit[]> {
+  return new Promise((resolve, reject) => {
+    const cwd = workspace.workspaceFolders?.at(0)?.uri.path || ".";
+    const result = cp.spawnSync("mix", ["format", "-"], {
+      cwd: cwd,
+      input: document.getText(),
+      encoding: "utf-8",
+    });
+    if (result.stderr === "" && result.stdout !== "") {
+      const textEditor = window.activeTextEditor;
+      if (!textEditor) {
+        return;
+      }
+      textEditor.edit((editBuilder) => {
+        editBuilder.replace(fullDocumentRange(document), result.stdout);
+      });
+    }
+  });
+}
+
+export function activate(context: ExtensionContext) {
+  languages.registerDocumentFormattingEditProvider("elixir", {
+    provideDocumentFormattingEdits(
+      document: TextDocument
+    ): Thenable<TextEdit[]> {
+      return document.save().then(() => {
+        return format(document);
+      });
+    },
+  });
+
+  let disposable = commands.registerCommand("mix-format.formatFile", () => {
+    const textEditor = window.activeTextEditor;
+    if (!textEditor) {
+      return;
+    }
+    format(textEditor.document);
+  });
+
+  context.subscriptions.push(disposable);
+}
